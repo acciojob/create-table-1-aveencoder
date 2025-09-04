3febfdb4f03ac5cdc4f47f819b5c90ab764c80b7
aveencoder <aveen598@gmail.com>
2025-09-04 21:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let i=1;
 function insert_Row() {
-    //Write your code here
-  jgkhikjijoj
+   let table = document.getElementById("sampleTable");
+  let cell1 = document.createElement('td');
+let cell2 = document.createElement('td');
+
+let row = document.createElement('tr');
+cell1.innerText = `New Cell${i}`;
+cell2.innerText = `New Cell${i+1}`;
+
+row.appendChild(cell1);
+row.appendChild(cell2);
+
+table.tBodies[0].insertBefore(row,table.rows[0]);
+  
+  i++;
   
 }
+
+
